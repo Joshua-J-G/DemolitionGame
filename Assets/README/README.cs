320eccd113549fbe0850980697740b8ce184c565
Damosolo <135564342+Damosolo@users.noreply.github.com>
2023-06-09 21:23:55 +1000

    new chair
    read the read me. also bombs must not of uploaded at uni

---
+++ Assets/README/README.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+    //DAMON: Josh ur res is out on my screen at home the Extra  buttons on the menu are half on the the menu.
+    //and after  i think the 5th round it cuts to an empty scene
+
+}
