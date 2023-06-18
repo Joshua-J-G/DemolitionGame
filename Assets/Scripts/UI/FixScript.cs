14d3a91a3bc99cffa91d4481f07feb1c8992dfe3
Damosolo <135564342+Damosolo@users.noreply.github.com>
2023-06-18 19:03:28 +1000

    Trying to get tactics running
    we will do it tomorrow but i need tactics set up so i can work on it i did my best with the stuff u gave me but i dont know how any of ur systems work

--- Assets/Scripts/UI/FixScript.cs
+++ Assets/Scripts/UI/FixScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FixScript : MonoBehaviour
 {
@@ -10,10 +11,10 @@ public class FixScript : MonoBehaviour
     }
     public void PlayTactics()
     {
-        Debug.Log("HEY YOU JUST TRIED PLAYING TACTICS enter this script for more");
-        //hey this is where you load up the first level of tactics when the play button is clicked it will do anything in this function
-        //as you can see above the startGame script is how i start bombrush you may want to look into that script and find out how this is loaded (may of forgoten how it works you'll be fine)
-        //ill add some comments to the code
+        // Load the "Tactics" scene
+        SceneManager.LoadScene("Tactics");
+
+
     }
 
     // Start is called before the first frame update
